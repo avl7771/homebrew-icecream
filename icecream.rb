2dc30614bacd4b9d0df0ff736fb8cb79910c2efc
Arjan van Leeuwen <arjanl@opera.com>
2018-01-03 15:32:26 +0100

    Change icecream to local (updated) versions and set default options
    We want man pages and clang rewriting, but we don't care about clang
    wrappers.

--- icecream.rb
+++ icecream.rb
@@ -1,35 +1,22 @@
 class Icecream < Formula
   desc "Distributed compiler with a central scheduler to share build load"
   homepage "https://en.opensuse.org/Icecream"
-  url "https://github.com/icecc/icecream/archive/1.1.tar.gz"
-  sha256 "92532791221d7ec041b7c5cf9998d9c3ee8f57cbd2da1819c203a4c6799ffc18"
-
-  bottle do
-    sha256 "ac7f6745981bd1c0853af12c4a460cd196a95b7c27c6072746db62086e6afcf0" => :high_sierra
-    sha256 "ff931dd74efc02cad494df41e2df0919fd1a65b2908ade15566b5a6f0974e3ee" => :sierra
-    sha256 "744922ad03cb2468d1b3251238f7130fb1e4295388370bf47d6edeb43a8c36b2" => :el_capitan
-    sha256 "0adc60662ea9ede33caf1fffb35a129593479a99c34bb36525c1c718b0a77639" => :yosemite
-  end
-
-  option "with-docbook2X", "Build with man page"
-  option "without-clang-wrappers", "Don't use symlink wrappers for clang/clang++"
-  option "with-clang-rewrite-includes", "Use by default Clang's -frewrite-includes option"
+  url "https://github.com/avl7771/icecream/archive/v1.2pre.tar.gz"
+  sha256 "4d11aced52ed256f3217b49c295c2d7bb825919c2cc0e129059897f77a554922"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "lzo"
-  depends_on "docbook2X" => [:optional, :build]
+  depends_on "docbook2X" => :build
 
   def install
     args = %W[
       --disable-dependency-tracking
       --disable-silent-rules
+      --enable-clang-write-includes
       --prefix=#{prefix}
     ]
-    args << "--without-man" if build.without? "docbook2X"
-    args << "--enable-clang-wrappers" if build.with? "clang-wrappers"
-    args << "--enable-clang-write-includes" if build.with? "clang-rewrite-includes"
 
     system "./autogen.sh"
     system "./configure", *args
