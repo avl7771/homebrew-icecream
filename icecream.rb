379eafb9d9c3fb5341a310b207e7899f25583a75
Arjan van Leeuwen <arjanl@opera.com>
2018-01-11 15:39:56 +0100

    Add plist definition to override default

--- icecream.rb
+++ icecream.rb
@@ -37,6 +37,24 @@ class Icecream < Formula
     EOS
   end
 
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+        <string>iceccd</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{sbin}/iceccd</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </plist>
+    EOS
+  end
+
+
   def iceccd_plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
